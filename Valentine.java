3c05e0cf9f04f45f313253ae62cfe2b593faf09f
Zhazha Mahootian <32941543+2H42H4@users.noreply.github.com>
2019-02-14 07:06:20 -0500

    Add files via upload

---
+++ Valentine.java
@@ -0,0 +1,20 @@
+
+package valentine;
+import java.util.Scanner;
+
+public class Valentine {
+    
+    public static void main(String[] args) {
+        System.out.println("Enter the category of valentine you would like: ");
+        Scanner input = new Scanner(System.in);
+        String category = input.nextLine();
+        String[] str_type_array = {"food", "animal", "flower", "music", "movie", "assorted"};
+        String[] food = {};
+        /*for(type in typeArray){
+            type.
+        */
+        
+        
+    }
+    
+}
